5e9eaf35590430d7b1bc15d4bdfd3418f5ad4ae9
Saurabh Kumar <saurabh.k@ipsator.com>
2026-06-03 21:47:55 +0530

    feat: Added Search functionality

--- src/applications/mini-ecommerce/components/Ecommerce.jsx
+++ src/applications/mini-ecommerce/components/Ecommerce.jsx
@@ -5,6 +5,7 @@ const Ecommerce = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(null);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     const controller = new AbortController();
@@ -27,7 +28,7 @@ const Ecommerce = () => {
           setIsError(error.message);
         }
       } finally {
-        if (controller.signal.aborted) {
+        if (!controller.signal.aborted) {
           setIsLoading(false);
         }
       }
@@ -40,28 +41,49 @@ const Ecommerce = () => {
   }, []);
 
   console.log("data", data);
+  const filteredData = data.filter((item) => {
+    const searchTerm = input.toLowerCase();
+    return (
+      item.title.toLowerCase().includes(searchTerm) ||
+      item.category.toLowerCase().includes(searchTerm)
+    );
+  });
 
   if (isLoading) return <h3>Loading.....</h3>;
   if (isError) return <div>{isError}</div>;
 
   return (
-    <div className="item-container">
-      {data.length > 0 &&
-        data.map((item) => (
-          <div className="item" key={item.id}>
-            <div className="image-container">
-              <img className="image" src={item.image} alt={item.category} />
+    <div>
+      <div className="input-container">
+        <input
+          className="input"
+          type="text"
+          placeholder="Enter title"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div className="item-container">
+        {filteredData.length === 0 ? (
+          <h2>No data available</h2>
+        ) : (
+          filteredData.map((item) => (
+            <div className="item" key={item.id}>
+              <div className="image-container">
+                <img className="image" src={item.image} alt={item.category} />
+              </div>
+              <h4 className="title">{item.title.slice(0, 50)}</h4>
+              <p className="category">{item.category}</p>
+              <p className="description">
+                {item.description.length >= 70
+                  ? item.description.slice(0, 70) + "...."
+                  : item.description}
+              </p>
+              <p className="price">{item.price}</p>
             </div>
-            <h4 className="title">{item.title.slice(0, 50)}</h4>
-            <p className="category">{item.category}</p>
-            <p className="description">
-              {item.description.length >= 70
-                ? item.description.slice(0, 70) + "...."
-                : item.description}
-            </p>
-            <p className="price">{item.price}</p>
-          </div>
-        ))}
+          ))
+        )}
+      </div>
     </div>
   );
 };
